929b31578f844bfb0b239cd405df8320f8818851
Emma Fish <143717596+duckiechip167@users.noreply.github.com>
2024-10-31 21:44:42 +0000

    Fix

--- script.js
+++ script.js
@@ -15,12 +15,6 @@ window.onload = function () {
 };
 
 function setMines() {
-  // minesLocation.push("2-2");
-  // minesLocation.push("2-3");
-  // minesLocation.push("5-6");
-  // minesLocation.push("3-4");
-  // minesLocation.push("1-1");
-
   let minesLeft = minesCount;
   while (minesLeft > 0) {
     let r = Math.floor(Math.random() * rows);
